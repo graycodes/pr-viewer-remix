ee9abbde67b136d9619e8586ea6d90e291dac7cd
Gray <gray@statsbomb.com>
2023-02-22 21:21:36 +0000

    switch to github lib for oauth

--- app/routes/oauth_callback.tsx
+++ app/routes/oauth_callback.tsx
@@ -1,49 +1,46 @@
-import { useLoaderData } from "@remix-run/react";
 import { redirect } from "@remix-run/node";
 import { cookieAccessToken, cookieUsername } from "~/cookie";
-
-export default function OauthCallBack() {
-  const x = useLoaderData();
-  return <p>hi</p>;
-}
+import { createOAuthAppAuth } from "@octokit/auth-oauth-app";
 
 export const loader = async ({ request: req }: { request: Request }) => {
   const params = new URL(req.url).searchParams;
   const code = params.get("code");
+
+  const {GITHUB_CLIENT_ID,GITHUB_SECRET, GITHUB_TOKEN, DEPLOY_URL } = process.env;
 
   if (!code) {
     console.error("no code set in oauth_callback");
     return null;
   }
 
-  const response = await fetch("https://github.com/login/oauth/access_token", {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      client_id: process.env.GITHUB_CLIENT_ID,
-      client_secret: process.env.GITHUB_SECRET,
-      code: code,
-      state: process.env.GITHUB_TOKEN,
-    }),
+  if (!GITHUB_CLIENT_ID || !GITHUB_SECRET || !GITHUB_TOKEN || !DEPLOY_URL) {
+    console.error('Missing environment variables!')
+    process.exit(1);
+  }
+
+  const auth = createOAuthAppAuth({
+    clientType: "oauth-app",
+    clientId: GITHUB_CLIENT_ID,
+    clientSecret: GITHUB_SECRET,
   });
 
-  const responseJson = await response.json();
-  const access_token = responseJson.access_token;
+  const {token} = await auth({
+    type: "oauth-user",
+    code: code,
+    state: GITHUB_TOKEN,
+  });
 
   const userRaw = await fetch("https://api.github.com/user", {
     headers: {
-      Authorization: `token ${access_token}`,
+      Authorization: `token ${token}`,
     },
   });
   const user = await userRaw.json();
   const username = user.login;
 
-  return redirect(process.env.DEPLOY_URL, {
+  return redirect(DEPLOY_URL, {
     headers: [
-      ["Set-Cookie", await cookieAccessToken.serialize(access_token)],
+      ["Set-Cookie", await cookieAccessToken.serialize(token)],
       ["Set-Cookie", await cookieUsername.serialize(username)],
     ],
   });
